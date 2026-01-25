f4f78360ddb8a0f4f7157935626430b78aac7d54
CheeseyHub <muhammedibrahim613@gmail.com>
2026-01-25 20:54:15 +0500

    Learned about binary search tree

--- python/datastructs/binary_tree.py
+++ python/datastructs/binary_tree.py
@@ -1,35 +1,20 @@
 from __future__ import annotations;
-from typing import   TypeVar,Generic, override;
+from typing import   TypeVar,Generic;
 T = TypeVar("T",int ,float);
 
 
 
-class list_acumalator(Generic[T]):
-    def __init__(self):
-        self.l:list[T]= [];
-        self.size:int = 0;
-
-    def add_to_list(self,node:TreeNode[T])->None:
-        self.l.append(node.val);
-        self.size+=1;
-
-    def get_value(self,index:int) ->T:
-        return self.l[index];
-    
             
         
-    @override
-    def __str__(self) -> str:
-        return str(self.l);
 
 
-class TreeNode(Generic[T]):
+class BST(Generic[T]):
     def __init__(self,data:T) -> None:
         self.val:T = data;
-        self.left:TreeNode[T] | None = None;
-        self.right:TreeNode[T] | None = None;
+        self.left:BST[T] | None = None;
+        self.right:BST[T] | None = None;
 
-    def max_depth(self,root: TreeNode[T] | None) ->int:
+    def max_depth(self,root: BST[T] | None) ->int:
         if root is None:
             return 0;
 
@@ -38,32 +23,46 @@ class TreeNode(Generic[T]):
 
         return 1 + max(left_depth,right_depth);
 
-    def total_nodes(self,root:TreeNode[T]|None) -> int :
+    def total_nodes(self,root:BST[T]|None) -> int :
         if root is None:
             return 0;
 
         return 1 + self.total_nodes(root.left) + self.total_nodes(root.right);
 
-    def traverse(self,root:TreeNode[T]|None,accu:list_acumalator[T]) ->None:
+    def traverse(self,root:BST[T]|None,accu:list[T]) ->None:
         if root is None:
             return;
 
         if root.left:
              self.traverse(root.left,accu);
 
-        accu.add_to_list(root)
+        accu.append(root.val)
         if root.right:
               self.traverse(root.right,accu);
 
 
-    def median(self,root:TreeNode[T]) ->  float :
-        accu:list_acumalator[T] = list_acumalator() 
+    def median(self,root:BST[T]) ->  float :
+        accu:list[T] = [];
         self.traverse(root,accu);
-        if accu.size % 2 != 0:
-            return accu.get_value(accu.size // 2);
+        size :int = len(accu)
+        if size % 2 != 0:
+            return accu[size//2 ];
         else:
-            return  (accu.get_value(accu.size // 2) + accu.get_value(( accu.size // 2 )  -1 )) / 2;
+            return  (accu[ size // 2 ] + accu[ ( size  // 2 )  - 1 ]) /2 ;
 
+    def insert(self,root:BST[T] | None,data:T) ->BST[T] | None:
+        if root is None:
+           return  BST(data);
+            
+            
+
+        assert(root is not None);
+        if data < root.val:
+            root.left = self.insert(root.left,data);
+        else:
+            root.right = self.insert(root.right,data);
+
+        return root;
 
 
         
@@ -74,28 +73,25 @@ class TreeNode(Generic[T]):
 
 
 
-def print_tree(node:TreeNode[T] |None) ->None:
+def print_tree(node:BST[T] |None) ->None:
     if node is None :
         return;
-    print(node.val);
     if node.left : 
         print_tree(node.left)
+    print(node.val);
     if node.right :
         print_tree(node.right);
 
 
 
 
-tree :TreeNode[int]=  TreeNode(100);
-tree.left = TreeNode(99) ;
-tree.left.left = TreeNode(1) ;
-tree.right = TreeNode(101) ;
-tree.right.right =TreeNode(9999)
+tree:BST[int] = BST(32);
+_ = tree.insert(tree,100);
+_ = tree.insert(tree,200);
 
 
+print_tree(tree);
 
-#print(tree.median(tree))
-print_tree(tree)
 
 
 
