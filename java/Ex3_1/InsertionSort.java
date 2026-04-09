d33b12edfe2c03ec6c50772a5b8e7b478a9f0dec
CheeseyHub <muhammedibrahim613@gmail.com>
2026-04-09 22:08:58 +0500

    did insertion sort

--- java/Ex3_1/InsertionSort.java
+++ java/Ex3_1/InsertionSort.java
@@ -1,3 +1,34 @@
 public class InsertionSort {
     
+
+    public static int[] insertionSort(int[] list)
+    {
+        for (int i = 1 ; i < list.length;i++)
+        {
+            int temp = list[i];
+            int j = i - 1;
+
+            while(j >= 0 && list[j] > temp)
+            {
+                list[j+1] = list[j];
+                j--;
+
+            }
+            list[j+1] = temp;
+        }
+        return list;
+    }
+    public static void main(String[] args) {
+        
+        int[] myList = {2,2,3,1,4,1,3,5};
+        int[] sorted = insertionSort(myList);
+        for(int i : sorted)
+        {
+            System.out.print(i+" ,");
+        }
+    }
+    
+
+
+    
 }
